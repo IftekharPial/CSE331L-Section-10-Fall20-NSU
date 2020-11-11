e264ed34b13fd73b3b9283d99a0eeda1eebd5160
1410213642-Pial <66737630+IftekharPial@users.noreply.github.com>
2020-11-11 12:35:21 +0600

    Files added

---
+++ Lab Assessment Submission/Lab 1/LAB 1 assesment.asm
@@ -0,0 +1,28 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h     ;#include<stdio.h> 
+             ;int main(){
+MOV AX,1             ; int a=2; 
+
+ 
+             ; int c = a+b;
+ADD AX,5             ; printf(c) ;
+             ; returnt 0;
+
+             ; } 
+ADD AX,5              
+
+
+
+
+
+
+; add your code here
+
+ret
+
+
+
+
